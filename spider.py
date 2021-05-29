a1565b0cf58aa2a07e84d42549dcefd52442fef6
HoRizon <jiatenghuipl@gmail.com>
2021-05-29 10:51:50 +0800

    test

---
+++ spider.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Project: pythonProject
+# @Author: HoRizon
+# @Time: 2021/5/27 10:44
+
+from bs4 import BeautifulSoup
+import re
+import urllib.request
+import urllib.parse
+import urllib.error
+import xlwt
+import sqlite3
+
+
+def get_html(url):
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36"}
+    request = urllib.request.Request(url=url, headers=headers, method='GET')
+    html = ""
+    try:
+        response = urllib.request.urlopen(request)
+        html = response.read().decode("utf-8").replace('&nbsp;', '')
+    except urllib.error.URLError as e:
+        if hasattr(e, "code"):
+            print(e.code)
+        if hasattr(e, "reason"):
+            print(e.reason)
+    return html
+
+
+if __name__ == "__main__":
+    a = get_html("https://movie.douban.com/top250?start=0")
+    print(a)
