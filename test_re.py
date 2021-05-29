6787af645b51770638c6d5b23c286aa22abb386d
HoRizon <jiatenghuipl@gmail.com>
2021-05-29 14:26:51 +0800

    test_re

--- test_re.py
+++ test_re.py
@@ -4,13 +4,12 @@
 # @Time: 2021/5/29 11:47
 
 import re
-pattern1 = re.compile(r'<p class="">\s*(.*)\s*\.\.\.<br/>', re.S)
+pattern1 = re.compile(r'<p class="">\s*(.*)<br/>', re.S)
 pattern2 = re.compile(r'<p class="">.*<br/>\s*(.*)</p>', re.S)
 text = '''<p class="">
-                            导演: 弗兰克·德拉邦特 Frank Darabont   主演: 蒂姆·罗宾斯 Tim Robbins /...<br>
+                            导演: 弗兰克·德拉邦特 Frank Darabont   主演: 蒂姆·罗宾斯 Tim Robbins /...<br/>
                             1994 / 美国 / 犯罪 剧情
                         </p>'''
-# result = pattern.search(text).group(1) + pattern.search(text).group(2)  <p class="">\s*(.*)\s*\.\.\.<br/>|
 result = pattern1.search(text).group(1) + pattern2.search(text).group(1)
 print(result)
 # <p class="">.*<br/>\s*(.*)</p>
